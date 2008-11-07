5f9fcb15e61f90a7476597df8a3d10ebdf9bc322
Pyrosim <Pyrosim@5c20fb76-a87a-11dd-8184-3158da6531e3>
2008-11-07 22:14:12 +0000

    A basic configuration mechanism. I'm doing the release *really* early release often approach. =p

---
+++ includes/header.php
@@ -0,0 +1,13 @@
+<?php
+
+echo <<<HEADER
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html>
+  <head>
+    <title>Lojban-com</title>
+  </head>
+  <body>
+
+HEADER;
+
+?>
